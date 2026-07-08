5fce21a3a3bd52a2eb6170bd9f69fcda1bbf600d
MaxMusicMax <45011326+MaxMusicMax@users.noreply.github.com>
2026-07-08 22:33:50 +0300

    Add files via upload

--- program.cs
+++ program.cs
@@ -6,20 +6,22 @@ using System.IO;
 using System.Text.Json;
 using System.Text.Encodings.Web;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 class MenuItem
 {
 	public string ItemName { get; set; } = "";
 	public string TypeMenu { get; set; } = "";
 	public string Icon { get; set; } = "";
-
 	public string ActionNameMenu { get; set; } = "";
 	public string ActionSnippet { get; set; } = "";
 	public string ActionOpenFolder { get; set; } = "";
 	public string ActionFolderScript { get; set; } = "";
 	public string ActionScriptSwitch { get; set; } = "";
 	public string ActionSettingsAhk { get; set; } = "";
-
+	public string ActionFontSize { get; set; } = "";
+	public string ActionIconSize { get; set; } = "";
 	public List<MenuItem> Children { get; set; } = new();
 }
 
@@ -31,14 +33,76 @@ class AppSettings
 	public string FolderIcon { get; set; } = "icons";
 	public string FolderSnippets { get; set; } = "snippets";
 	public bool AlwaysOnTop { get; set; } = false;
+	public int FontSize { get; set; } = 12;
+	public int IconSize { get; set; } = 24;
 }
 
 class Program
 {
-	
+	const int HOTKEY_ID = 1;
+	const int WM_HOTKEY = 0x0312;
+
+	[DllImport("user32.dll")]
+	static extern bool RegisterHotKey(
+		IntPtr hWnd,
+		int id,
+		uint fsModifiers,
+		uint vk
+	);
+
+	[DllImport("user32.dll")]
+	static extern bool UnregisterHotKey(
+		IntPtr hWnd,
+		int id
+	);
+
+	[DllImport("user32.dll")]
+	static extern IntPtr GetForegroundWindow();
+
+	[DllImport("user32.dll")]
+	static extern bool SetForegroundWindow(IntPtr hWnd);
+
+	[DllImport("user32.dll")]
+	static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+
+	const byte VK_CONTROL = 0x11;
+	const byte VK_V = 0x56;
+	const uint KEYEVENTF_KEYUP = 0x0002;
+
+	static NotifyIcon tray = null!;
+	static ContextMenuStrip menu = null!;
+	static ChangeHotkeyForm? hotkeyForm;
+	static List<MenuItem> menuItems = new();
+
 	static string AppPath = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)!.Parent!.Parent!.Parent!.FullName;
 	static string ConfigFile = Path.Combine(AppPath, "config.ini");
-	
+
+	class MyContextMenuStrip : ContextMenuStrip
+	{
+		protected override CreateParams CreateParams
+		{
+			get
+			{
+				CreateParams cp = base.CreateParams;
+				cp.ExStyle |= 0x80; // WS_EX_TOOLWINDOW
+				return cp;
+			}
+		}
+	}
+
+	class MyToolStripDropDownMenu : ToolStripDropDownMenu
+	{
+		protected override CreateParams CreateParams
+		{
+			get
+			{
+				CreateParams cp = base.CreateParams;
+				cp.ExStyle |= 0x80; // WS_EX_TOOLWINDOW
+				return cp;
+			}
+		}
+	}
+
 	[STAThread]
 	static void Main()
 	{
@@ -53,8 +117,6 @@ class Program
 
 		string jsonFile = Path.ChangeExtension(menuFile, ".json");
 
-		List<MenuItem> menuItems;
-
 		if (File.Exists(jsonFile))
 		{
 			menuItems = JsonSerializer.Deserialize<List<MenuItem>>(
@@ -66,14 +128,33 @@ class Program
 			menuItems = LoadMenuCache(settings.CurrentMenu);
 		}
 
-		ContextMenuStrip menu = new ContextMenuStrip();
+		menu = new MyContextMenuStrip();
+
+		menu.ImageScalingSize = new Size(
+			settings.IconSize,
+			settings.IconSize
+		);
+
+		menu.Font = new Font(
+			menu.Font.FontFamily,
+			settings.FontSize
+		);
+
+		menu.ImageScalingSize = new Size(18, 18);
 
 		BuildMenu(menu.Items, menuItems);
 
-		NotifyIcon tray = new NotifyIcon();
+		tray = new NotifyIcon();
 		tray.Icon = System.Drawing.SystemIcons.Application;
-		tray.Visible = true;
 		tray.ContextMenuStrip = menu;
+		tray.Visible = !settings.HideIcon;
+
+		RegisterHotKey(
+			ProgramMessageWindow.Handle,
+			HOTKEY_ID,
+			ParseModifiers(settings.Hotkey),
+			ParseKey(settings.Hotkey)
+		);
 
 		Application.Run();
 	}
@@ -138,6 +219,8 @@ class Program
 			item.ActionFolderScript = GetAttribute(child, "action_folder_script");
 			item.ActionScriptSwitch = GetAttribute(child, "action_script_switch");
 			item.ActionSettingsAhk = GetAttribute(child, "action_settings_ahk");
+			item.ActionFontSize = GetAttribute(child, "action_font_size");
+			item.ActionIconSize = GetAttribute(child, "action_icon_size");
 
 			if (item.TypeMenu == "subMenu")
 				item.Children = LoadMenu(child);
@@ -160,8 +243,25 @@ class Program
 				continue;
 			}
 
-			ToolStripMenuItem menuItem =
-				new ToolStripMenuItem(item.ItemName);
+			ToolStripMenuItem menuItem = new ToolStripMenuItem(item.ItemName);
+			
+			if (item.Icon != "")
+			{
+				menuItem.Image = LoadIcon(item.Icon);
+			}
+			else if (item.ActionOpenFolder != "")
+			{
+				string file = Environment.ExpandEnvironmentVariables(
+					item.ActionOpenFolder
+				);
+
+				if (File.Exists(file))
+				{
+					menuItem.Image = LoadFileIcon(file);
+				}
+			}
+
+			menuItem.DropDown = new MyToolStripDropDownMenu();
 
 			if (item.TypeMenu == "subMenu")
 			{
@@ -181,13 +281,132 @@ class Program
 					};
 				}
 			}
-
+			menuItem.Font = menu.Font;
 			collection.Add(menuItem);
+		}
+	}
+
+	static void SetMenuFont(
+	ToolStripItemCollection items,
+	Font font)
+	{
+		foreach (ToolStripItem item in items)
+		{
+			item.Font = font;
+
+			if (item is ToolStripMenuItem menuItem)
+			{
+				SetMenuFont(
+					menuItem.DropDownItems,
+					font
+				);
+			}
+		}
+	}
+
+	static void ReloadMenu()
+	{
+		AppSettings settings = LoadSettings();
+
+		string jsonFile = Path.Combine(
+			AppPath,
+			Path.ChangeExtension(settings.CurrentMenu, ".json")
+		);
+
+		if (File.Exists(jsonFile))
+		{
+			menuItems = JsonSerializer.Deserialize<List<MenuItem>>(
+				File.ReadAllText(jsonFile)
+			) ?? new List<MenuItem>();
+		}
+		else
+		{
+			menuItems = LoadMenuCache(settings.CurrentMenu);
+		}
+
+		menu.Items.Clear();
+
+		BuildMenu(
+			menu.Items,
+			menuItems
+		);
+	}
+
+	static void SetMenuIconSize(int size)
+	{
+		menu.ImageScalingSize = new Size(size, size);
+
+		SetDropDownIconSize(
+			menu.Items,
+			size
+		);
+	}
+
+	static void SetDropDownIconSize(
+	ToolStripItemCollection items,
+	int size)
+	{
+		foreach (ToolStripItem item in items)
+		{
+			item.ImageScaling =
+				ToolStripItemImageScaling.SizeToFit;
+
+
+			if (item is ToolStripMenuItem menuItem)
+			{
+				menuItem.DropDown.ImageScalingSize =
+					new Size(size, size);
+
+				SetDropDownIconSize(
+					menuItem.DropDownItems,
+					size
+				);
+			}
 		}
 	}
 
 	static void ExecuteAction(MenuItem item)
 	{
+		
+		if (item.ActionIconSize != "")
+		{
+			AppSettings settings = LoadSettings();
+
+			settings.IconSize =
+				int.Parse(item.ActionIconSize);
+
+			SaveSettings(settings);
+
+			SetMenuIconSize(
+				settings.IconSize
+			);
+
+			return;
+		}
+
+		if (item.ActionFontSize != "")
+		{
+			AppSettings settings = LoadSettings();
+
+			settings.FontSize = int.Parse(
+				item.ActionFontSize
+			);
+
+			SaveSettings(settings);
+
+			menu.Font = new Font(
+				menu.Font.FontFamily,
+				settings.FontSize
+			);
+
+			SetMenuFont(
+				menu.Items,
+				menu.Font
+			);
+
+			return;
+		}
+		
 		if (item.ActionSnippet != "")
 		{
 			string text = item.ActionSnippet;
@@ -208,40 +427,60 @@ class Program
 				);
 			}
 
+			IntPtr hwnd = GetForegroundWindow();
 			Clipboard.SetText(text);
-			SendKeys.SendWait("^v");
+			Thread.Sleep(100);
+			SetForegroundWindow(hwnd);
+			Thread.Sleep(100);
+			keybd_event(VK_CONTROL, 0, 0, UIntPtr.Zero);
+			keybd_event(VK_V, 0, 0, UIntPtr.Zero);
+			keybd_event(VK_V, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
+			keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
+
 		}
+
 		else if (item.ActionOpenFolder != "")
 		{
-			
 			string command = Environment.ExpandEnvironmentVariables(
 				item.ActionOpenFolder
 			);
 
-			string fileName;
-			string arguments = "";
-
-			int space = command.IndexOf(' ');
-
-			if (space > 0)
+			if (File.Exists(command) || Directory.Exists(command))
 			{
-				fileName = command.Substring(0, space);
-				arguments = command.Substring(space + 1);
+				Process.Start(
+					new ProcessStartInfo
+					{
+						FileName = command,
+						UseShellExecute = true
+					});
 			}
 			else
 			{
-				fileName = command;
-			}
+				string fileName;
+				string arguments = "";
 
-			System.Diagnostics.Process.Start(
-				new System.Diagnostics.ProcessStartInfo
+				int space = command.IndexOf(' ');
+
+				if (space > 0)
 				{
-					FileName = fileName,
-					Arguments = arguments,
-					UseShellExecute = true
-				});
+					fileName = command.Substring(0, space);
+					arguments = command.Substring(space + 1);
+				}
+				else
+				{
+					fileName = command;
+				}
 
+				Process.Start(
+					new ProcessStartInfo
+					{
+						FileName = fileName,
+						Arguments = arguments,
+						UseShellExecute = true
+					});
+			}
 		}
+
 		else if (item.ActionFolderScript != "")
 		{
 			string folder = item.ActionFolderScript;
@@ -268,8 +507,148 @@ class Program
 		}
 		else if (item.ActionSettingsAhk != "")
 		{
-			MessageBox.Show(
-				"Setting: " + item.ActionSettingsAhk);
+			
+			if (item.ActionSettingsAhk == "DeleteJson")
+			{
+				AppSettings settings = LoadSettings();
+				string jsonFile = Path.Combine(
+					AppPath,
+					Path.ChangeExtension(settings.CurrentMenu, ".json")
+				);
+
+				if (File.Exists(jsonFile))
+				{
+					File.Delete(jsonFile);
+				}
+
+				ReloadMenu();
+			}
+
+			else if (item.ActionSettingsAhk == "IconShow")
+			{
+				AppSettings settings = LoadSettings();
+				settings.HideIcon = !settings.HideIcon;
+				SaveSettings(settings);
+				tray.Visible = !settings.HideIcon;
+			}
+
+			else if (item.ActionSettingsAhk == "CloseProgram")
+			{
+				tray.Visible = false;
+				Application.Exit();
+			}
+
+			else if (item.ActionSettingsAhk == "RebootProgram")
+			{
+				tray.Visible = false;
+				Application.Restart();
+			}
+
+			else if (item.ActionSettingsAhk == "ChangeHotkeyGui")
+			{
+				ChangeHotkeyForm form = new ChangeHotkeyForm();
+				form.ShowDialog();
+			}
+
+			else
+			{
+				MessageBox.Show(
+					"Setting: " + item.ActionSettingsAhk);
+			}
+		}
+	}
+
+	[DllImport("shell32.dll", CharSet = CharSet.Auto)]
+	static extern uint ExtractIconEx(
+		string szFileName,
+		int nIconIndex,
+		IntPtr[] phiconLarge,
+		IntPtr[] phiconSmall,
+		uint nIcons
+	);
+
+	[DllImport("user32.dll")]
+	static extern bool DestroyIcon(
+		IntPtr hIcon
+	);
+
+	static System.Drawing.Image? LoadIcon(string icon)
+	{
+		try
+		{
+			string[] parts = icon.Split(':');
+
+			string file = parts[0];
+			int index = 0;
+
+			if (parts.Length > 1)
+				index = int.Parse(parts[1]) - 1;
+
+			AppSettings settings = LoadSettings();
+
+			file = Path.Combine(
+				AppPath,
+				settings.FolderIcon,
+				file
+			);
+
+			if (!File.Exists(file))
+				return null;
+
+			IntPtr[] large = new IntPtr[1];
+			IntPtr[] small = new IntPtr[1];
+
+			uint result = ExtractIconEx(
+				file,
+				index,
+				large,
+				small,
+				1
+			);
+
+			if (result == 0)
+				return null;
+
+			System.Drawing.Icon ico =
+				System.Drawing.Icon.FromHandle(
+					small[0] != IntPtr.Zero
+					? small[0]
+					: large[0]
+				);
+
+			System.Drawing.Image image =
+				ico.ToBitmap();
+
+
+			DestroyIcon(
+				small[0] != IntPtr.Zero
+				? small[0]
+				: large[0]
+			);
+
+			return image;
+		}
+		catch
+		{
+			return null;
+		}
+	}
+
+	static System.Drawing.Image? LoadFileIcon(string file)
+	{
+		try
+		{
+			using System.Drawing.Icon? icon =
+				System.Drawing.Icon.ExtractAssociatedIcon(file);
+
+			if (icon == null)
+				return null;
+
+			return icon.ToBitmap();
+		}
+		catch
+		{
+			return null;
 		}
 	}
 
@@ -277,7 +656,6 @@ class Program
 	{
 		return node.Attributes?[name]?.Value ?? "";
 	}
-
 
 	static AppSettings LoadSettings()
 	{
@@ -303,7 +681,7 @@ AlwaysOnTop=false";
 			return settings;
 		}
 
-		foreach (string line in File.ReadAllLines(ConfigFile))
+		foreach (string line in File.ReadAllLines(ConfigFile).Select(x => x.Trim()))
 		{
 			if (line.StartsWith("Hotkey="))
 				settings.Hotkey = line["Hotkey=".Length..];
@@ -324,27 +702,75 @@ AlwaysOnTop=false";
 				settings.AlwaysOnTop =
 					line["AlwaysOnTop=".Length..]
 					.Equals("true", StringComparison.OrdinalIgnoreCase);
+			
+			else if (line.StartsWith("FontSize="))
+			settings.FontSize =
+				int.Parse(line["FontSize=".Length..]);
+
+			else if (line.StartsWith("IconSize="))
+			settings.IconSize =
+				int.Parse(line["IconSize=".Length..]);
+			
 		}
 
 		return settings;
 	}
 
+	static uint ParseModifiers(string hotkey)
+	{
+		uint modifiers = 0;
+
+		if (hotkey.Contains("Ctrl"))
+			modifiers |= 0x0002;
+
+		if (hotkey.Contains("Shift"))
+			modifiers |= 0x0004;
+
+		if (hotkey.Contains("Alt"))
+			modifiers |= 0x0001;
+
+		if (hotkey.Contains("Win"))
+			modifiers |= 0x0008;
+
+		return modifiers;
+	}
+
+	static uint ParseKey(string hotkey)
+	{
+		string key = hotkey
+			.Split('+')
+			.Last();
+
+		if (key.Length == 1)
+		{
+			return (uint)char.ToUpper(key[0]);
+		}
+
+		if (key.StartsWith("F"))
+		{
+			return (uint)Keys.F1 +
+				(uint.Parse(key.Substring(1))) - 1;
+		}
+
+		return (uint)Keys.None;
+	}
 
 	static void SaveSettings(AppSettings settings)
 	{
 		File.WriteAllText(
 			ConfigFile,
-			$@"[Settings]
-	Hotkey={settings.Hotkey}
-	HideIcon={(settings.HideIcon ? "1" : "0")}
-	CurrentMenu={settings.CurrentMenu}
-	FolderIcon={settings.FolderIcon}
-	FolderSnippets={settings.FolderSnippets}
-	AlwaysOnTop={(settings.AlwaysOnTop ? "true" : "false")}
-	"
+$@"[Settings]
+Hotkey={settings.Hotkey}
+HideIcon={(settings.HideIcon ? "1" : "0")}
+CurrentMenu={settings.CurrentMenu}
+FolderIcon={settings.FolderIcon}
+FolderSnippets={settings.FolderSnippets}
+AlwaysOnTop={(settings.AlwaysOnTop ? "true" : "false")}
+FontSize={settings.FontSize}
+IconSize={settings.IconSize}
+"
 		);
 	}
-
 
 	static void SwitchMenu(string menuName)
 	{
@@ -354,5 +780,151 @@ AlwaysOnTop=false";
 		Application.Restart();
 	}
 
+	static void CheckJson()
+	{
+		AppSettings settings = LoadSettings();
+
+		string xmlFile = Path.Combine(
+			AppPath,
+			settings.CurrentMenu
+		);
+
+		string jsonFile = Path.ChangeExtension(
+			xmlFile,
+			".json"
+		);
+
+		if (!File.Exists(jsonFile))
+		{
+			LoadMenuCache(settings.CurrentMenu);
+		}
+	}
+
+	static NativeWindow ProgramMessageWindow = new HotkeyWindow();
+
+	class HotkeyWindow : NativeWindow
+	{
+		public HotkeyWindow()
+		{
+			CreateParams cp = new CreateParams();
+
+			cp.ExStyle = 0x00000080; // WS_EX_TOOLWINDOW
+			cp.Style = unchecked((int)0x80000000); // WS_POPUP
+
+			CreateHandle(cp);
+		}
+
+		protected override void WndProc(ref Message m)
+		{
+			if (m.Msg == WM_HOTKEY)
+			{
+				CheckJson();
+
+				menu.Show(Cursor.Position);
+			}
+
+			base.WndProc(ref m);
+		}
+	}
+
+	class ChangeHotkeyForm : Form
+	{
+		TextBox box;
+		Button save;
+
+		public ChangeHotkeyForm()
+		{
+			Text = "Горячие клавиши";
+			Width = 300;
+			Height = 150;
+			StartPosition = FormStartPosition.CenterScreen;
+
+
+			AppSettings settings = LoadSettings();
+
+
+			Label label = new Label();
+			label.Text = "Новая комбинация:";
+			label.Left = 10;
+			label.Top = 15;
+			label.Width = 200;
+
+
+			box = new TextBox();
+			box.Left = 10;
+			box.Top = 40;
+			box.Width = 250;
+			box.Text = settings.Hotkey;
+			box.ReadOnly = true;
+
+
+			box.KeyDown += (s, e) =>
+			{
+				List<string> keys = new();
+
+				if (e.Control)
+					keys.Add("Ctrl");
+
+				if (e.Shift)
+					keys.Add("Shift");
+
+				if (e.Alt)
+					keys.Add("Alt");
+
+
+				if (e.KeyCode >= Keys.A &&
+					e.KeyCode <= Keys.Z)
+				{
+					keys.Add(e.KeyCode.ToString());
+				}
+				else if (e.KeyCode >= Keys.F1 &&
+					e.KeyCode <= Keys.F12)
+				{
+					keys.Add(e.KeyCode.ToString());
+				}
+
+
+				box.Text = string.Join("+", keys);
+			};
+
+
+			save = new Button();
+			save.Text = "Сохранить";
+			save.Left = 10;
+			save.Top = 75;
+			save.Width = 100;
+
+
+			save.Click += (s, e) =>
+			{
+				AppSettings settings = LoadSettings();
+
+				settings.Hotkey = box.Text;
+
+				SaveSettings(settings);
+
+				UnregisterHotKey(
+					ProgramMessageWindow.Handle,
+					HOTKEY_ID
+				);
+
+
+				RegisterHotKey(
+					ProgramMessageWindow.Handle,
+					HOTKEY_ID,
+					ParseModifiers(settings.Hotkey),
+					ParseKey(settings.Hotkey)
+				);
+
+
+				Close();
+			};
+
+
+			Controls.Add(label);
+			Controls.Add(box);
+			Controls.Add(save);
+		}
+	}
 
 }
